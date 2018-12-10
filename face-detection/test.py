63de958311b84778847f844cae9c75ee69251258
saisubramani <saisubramani68@gmail.com>
2018-12-10 16:17:19 +0530

    face-count-gui

--- face-detection/test.py
+++ face-detection/test.py
@@ -1,3 +1,5 @@
+#AUTHOR : J SAI SUBRAMANI
+
 import tkinter
 import cv2 
 from PIL import Image,ImageTk
@@ -5,6 +7,11 @@ import time
 from tkinter import filedialog
 from tkinter import END
 import imutils
+import mxnet as mx
+import align.detect_face
+from mtcnn_detector import MtcnnDetector
+
+count = 0
 
 class App:
     def __init__(self, window, window_title):
@@ -12,26 +19,26 @@ class App:
         self.window = window
         self.window.geometry('1000x1000')
         self.window.title(window_title)
-        #self.video_source = video_source
- 
-       # open video source (by default this will try to open the computer webcam)
-        #self.vid = MyVideoCapture()#self.video_source)
+        
          
        # Create a canvas that can fit the above video source size
         self.canvas = tkinter.Canvas(window, width = 500, height = 500)
-        #self.canvas.pack()
         self.canvas.grid(column =0, row =10)
  
        
         self.label=tkinter.Label(window, text="choose MP4 file")
-        #self.label.pack(anchor=tkinter.CENTER, expand=True)
         self.label.grid(column = 0, row=0)
 
         self.label1=tkinter.Entry(window, text="", width=70)
-        self.label1.grid(column=10,row=0)
+        self.label1.grid(column=0,row=8)
+
+        self.label2=tkinter.Label(window,text="number of face detected")
+        self.label2.grid(column=1,row=20)
+
+        self.no_face=tkinter.Entry(window,text="",width =20)
+        self.no_face.grid(column=2,row=20)
         
         self.btn_browse=tkinter.Button(window,text="BROWSE",width=20,command=self.browse_button)
-        #self.btn_browse.pack(anchor=tkinter.CENTER, expand=True)
         self.btn_browse.grid(column =0, row =5)
        # After it is called once, the update method will be automatically called every delay milliseconds
         self.delay = 15
@@ -49,6 +56,9 @@ class App:
     
     def update(self):
        # Get a frame from the video source
+       global count
+       self.no_face.delete(0,END)
+       self.no_face.insert(0,count)
        ret, frame = self.vid.get_frame()
  
        if ret:
@@ -61,19 +71,54 @@ class App:
 class MyVideoCapture:
     def __init__(self, video_source):
         # Open the video source
-        
+        self.detector = MtcnnDetector(model_folder='model', ctx=mx.cpu(0), num_worker = 4 , accurate_landmark = False)
         self.vid = cv2.VideoCapture(video_source)
         if not self.vid.isOpened():
             raise ValueError("Unable to open video source", video_source)
  
-        # Get video source width and height
-        #self.width = self.vid.get(cv2.CAP_PROP_FRAME_WIDTH)
-        #self.height = self.vid.get(cv2.CAP_PROP_FRAME_HEIGHT)
+        
  
     def get_frame(self):
         if self.vid.isOpened():
             ret, frame = self.vid.read()
-            frame = cv2.resize(frame,(500,500),interpolation = cv2.INTER_AREA)
+            img = cv2.resize(frame, (640,640))
+            (h, w) = img.shape[:2]
+
+
+            results = self.detector.detect_face(img)
+    
+            
+            
+            if results != None:
+              total_boxes = results[0]
+              points = results[1]
+              global count
+              count = 0
+      
+              draw = img.copy()
+              for b in total_boxes:
+                cv2.rectangle(draw, (int(b[0]), int(b[1])), (int(b[2]), int(b[3])), (255, 255, 255))
+                x1 = int(b[0])
+                y1 = int(b[1])
+                x2 = int(b[2])
+                y2 = int(b[3])
+
+                centroid = (int((x1+x2)/2),int((y1+y2)/2))
+              
+                if centroid[0]>=60 and centroid[0]<=w-60:
+                  
+                
+                  count += 1
+                  cv2.circle(draw, centroid, 4, (0, 255, 0), -1)
+                  print("no_face:",count)
+                  cv2.putText(draw, "count = "+str(count), (0, 20),cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
+                
+
+                  frame = cv2.resize(draw,(500,500),interpolation = cv2.INTER_AREA)
+
+
+
+                  frame = cv2.resize(frame,(500,500),interpolation = cv2.INTER_AREA)
             if ret:
                 # Return a boolean success flag and the current frame converted to BGR
                 return (ret, cv2.cvtColor(frame, cv2.COLOR_BGR2RGB))
